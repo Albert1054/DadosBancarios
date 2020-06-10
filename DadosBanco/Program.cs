b4e95f8c193e0d2161bb491f35f8dce2070fceb4
Albert Cavalcanti <cavalcanti_alberto@yahoo.com.br>
2020-06-10 18:10:18 -0300

    inicio de projeto

---
+++ DadosBanco/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DadosBanco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
